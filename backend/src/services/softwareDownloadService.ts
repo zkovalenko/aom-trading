8282fe4ee2a840938b2d440ee9ac1460a7861260
Zhenya Minkovich <zhenya@minkovich.com>
2025-09-30 15:50:53 -0700

    updating google client id to not collide

--- backend/src/services/softwareDownloadService.ts
+++ backend/src/services/softwareDownloadService.ts
@@ -27,7 +27,7 @@ function getDriveClient() {
       private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
       private_key: privateKey,
       client_email: clientEmail,
-      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_id: process.env.GOOGLE_DRIVE_CLIENT_ID,
       auth_uri: process.env.GOOGLE_AUTH_URI,
       token_uri: process.env.GOOGLE_TOKEN_URI,
       auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER,
